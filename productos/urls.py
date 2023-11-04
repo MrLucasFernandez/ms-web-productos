872b57d5ef04343f70c050bcb3cf0e4c63277c50
MrLucasFernandez <lucasfernandez.mu@gmail.com>
2023-11-04 00:35:50 -0300

    Implementación de CRUD

--- productos/urls.py
+++ productos/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     #path('',views.inicio, name=''),
     path('',views.ListarProductos, name='productos'),
+    path('detalle/<str:pk>',views.FiltrarProducto, name='detalle'),
+    path('crear', views.CrearProducto, name="crear"),
+    path('actualizar/<str:pk>/', views.ActualizarProducto, name="actualizar"),
+    path('eliminar/<str:pk>/', views.EliminarProducto, name="eliminar"),
     ]
--- productos/views.py
+++ productos/views.py
@@ -15,3 +15,39 @@ def ListarProductos(request):
     productos = Producto.objects.all()
     serializer = ProductosSerializer(productos, many=True)
     return Response(serializer.data)
+
+@api_view(["GET"])
+def FiltrarProducto(request, pk):
+    productos = Producto.objects.get(id=pk)
+    serializer = ProductosSerializer(productos, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def CrearProducto(request):
+    serializer = ProductosSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    else:
+        return Response(serializer.errors)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def ActualizarProducto(request, pk):
+    productos = Producto.objects.get(id=pk)
+    serializer = ProductosSerializer(instance=productos, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    else:
+        return Response(serializer.errors)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def EliminarProducto(request, pk):
+    productos = Producto.objects.get(id=pk)
+    productos.delete()
+
+    return Response('Eliminado')
